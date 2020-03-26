ecc622215aaa5f8bb696f7a38aab007038e4ef78
alexis-houpert <houpert.alexis@gmail.com>
2020-03-26 09:47:25 +0100

    push num 3

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 
-//$obj = new stdClass;
-//$obj->success = false;
-//$obj->message = 'Erreur, le username ou le mdp il faut se débrouiller dans la vie';
+
+$obj = new stdClass;
+$obj->success = false;
+$obj->message = 'Erreur, pas de correspondance  id mdp. Il faut se débrouiller dans la vie';
 
 
 try
@@ -16,48 +17,71 @@ catch (Exception $e){
 
 }
 
+echo $_POST['submit'];
+echo $_POST['id'];
+echo $_POST['mdp'];
+
 if (isset($_POST['submit']))
 {
-    if (!empty($_POST['id']) && !empty($_POST['mdp']))
+    if( !isset($_POST['id']) && !isset($_POST['mdp']) ){
+        echo "<span class='invalid-feedback'>Les variables id et mdp ne sont pas définis!</span>";
+    }
+        $id = htmlspecialchars($_GET['id']);
+        $mdp = htmlspecialchars($_GET['mdp']);
+
+        $errorEmpty = false;
+        $errorId = false;
+
+        if (empty($id) || empty($mdp)) {
+            echo "<span class='invalid-feedback'>Remplissez toutes les cases !</span>"; //déplacer ce code dans le js
+            $errorEmpty = true;
+        } elseif (filter_var($id, FILTER_VALIDATE_EMAIL)) {
+            echo "<span class='invalid-feedback'>Entrez un mail valide !</span>"; //depalcer ce
+            $errorId = true;
+        } else {
+            echo "<span class='valid-feedback'>Formulaire valide !</span>";
+        }
+
+        //VERIF BD
+
+    $cryptmdp = sha1($mdp);
+
+
+    $req = $bdd->prepare('SELECT id, mdp FROM user WHERE user.id = ? AND user.mdp = ?');
+    $req->execute($id, $cryptmdp);
+
+
+    $donnees = $req->fetch();
+
+
+
+    $found = isset($donnees['id'] ) && isset($donnees['mdp']);
+    if ($found)
     {
-        $id = htmlspecialchars($_POST['id']);
-        $mdp = htmlspecialchars($_POST['mdp']);
-
-        $cryptmdp = sha1($mdp);
-
-
-        $req = $bdd->prepare('SELECT id, mdp FROM user WHERE user.id = ? AND user.mdp = ?');
-        $req->execute($id, $cryptmdp);
-
-
-        $donnees = $req->fetch();
-
-
-
-        $found = isset($donnees['id'] ) && isset($donnees['mdp']);
-        if ($found)
-        {
-            //$obj -> success = true;
-            $_SESSION[$id] = $id;
-        }
-        else
-        {
-            echo "Pas de correspondance pour l'id et/ou le mot de passe";
-        }
+        session_start();
+        $_SESSION[$id] = $id;
+        $obj -> success = true;
     }
     else
     {
-        echo'Les champs envoyés sont vides';
+        echo "<span class='invalid-feedback'>Pas de correspondance pour l'id et/ou le mot de passe. PHP side</span>";
     }
+
 }
-else
-{
-    echo'Erreur : ';
-}
+    else {
+        echo "<span class='invalid-feedback'>Erreur lors de l'envoi du formulaire !</span>";
+    }
 
 
-header('Cache-Control: no-cache, must-revalidate');
+
+
+
+
+
+
+/*header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-echo json_encode($obj);
+json_encode($obj); */
+
--- script/login.js
+++ script/login.js
@@ -1,18 +1,50 @@
 (function() {
     'use strict';
     $(() => {
-        $('#form-login').submit(function ()
+        $('#mail-submit').click(function (event)
         {
-            $('#message').fadeOut();
+            event.preventDefault();
 
-            $.ajax({
-                url: $(this).attr('action'), //on peut ajouter l
-                method: $(this).attr('methode'),
-                data: $(this).serialize()
+            let id = $("#mail-id").val();
+            let mdp = $("#mail-mdp").val();
+            let submit = $("#mail-submit").val();
+
+            $.post(
+                'login.php', // Un script PHP que l'on va créer juste après
+                {
+                    id : id,  // Nous récupérons la valeur de nos inputs que l'on fait passer à connexion.php
+                    mdp : mdp,
+                    submit : submit
+                },
+
+                function(data){ // Cette fonction ne fait rien encore, nous la mettrons à jour plus tard
+                    if(data == 'Success'){
+                        // Le membre est connecté. Ajoutons lui un message dans la page HTML.
+
+                        $("#message").html("<p class='invalid-feedback'>Vous avez été connecté avec succès !</p>");
+                    }
+                    else{
+                        // Le membre n'a pas été connecté. (data vaut ici "failed")
+
+                        $("#message").html("<p class='invalid-feedback'>Erreur lors de la connexion...</p>");
+                    }
+                    },
+               // Nous souhaitons recevoir "Success" ou "Failed", donc on indique text !
+            );
+
+            /*$.ajax({
+                url: $(this).attr('action'),
+                type: $(this).attr('methode'),
+                data: JSON.stringify({
+                    submit : submit,
+                    id : id,
+                    mdp : mdp
+                }),
+                dataType: 'json'
             }).done(function (data) {
                 if (data.success === true)
                 {
-                    window.location.href = '../login.php';
+                    window.location.href = '../userpage.php';
                 }
                 else
                 {
@@ -21,7 +53,7 @@
             }).fail(function () {
                 $('body').html('Une erreur réseau est arrivée, veuillez réessayer');
             });
-            return false;
+            return false;*/
         })
     });
 }) ();
